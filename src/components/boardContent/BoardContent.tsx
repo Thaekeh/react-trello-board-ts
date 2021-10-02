cd7c49c0e9a35f390d19dbb1be5a4980ff498649
Thaeke Hekkenberg <thaekeh@gmail.com>
2021-10-02 19:24:02 +0200

    Load columns in BoardContent.tsx

--- src/components/boardContent/BoardContent.tsx
+++ src/components/boardContent/BoardContent.tsx
@@ -1,22 +1,36 @@
 import React from "react";
 import { BoardData } from "../../Data/initialBoardData";
-import { Column } from "../Column/Column";
+import { Column } from "../column/Column";
 import "./BoardContent.css";
 
 interface Props {
   boardData: BoardData;
+  onCreateNewCard: (cardTitle: string) => void;
 }
 
-export const BoardContent: React.FC<Props> = ({ boardData }) => {
+export const BoardContent: React.FC<Props> = ({
+  boardData,
+  onCreateNewCard,
+}) => {
   const columns = boardData.columnOrder.map(
     (column) => boardData.columns[column]
   );
 
   return (
     <div className="board__content">
-      {columns.map((column) => (
-        <Column key={column.id} column={column} />
-      ))}
+      {columns.map((column) => {
+        const tickets = column.tickets.map(
+          (ticket) => boardData.tickets[ticket]
+        );
+        return (
+          <Column
+            key={column.id}
+            column={column}
+            tickets={tickets}
+            onCreateNewCard={onCreateNewCard}
+          />
+        );
+      })}
     </div>
   );
 };
