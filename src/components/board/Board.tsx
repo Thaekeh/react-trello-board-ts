1e21ac6ae04b73f5103ddb53a1100cdaaa3984f9
Thaeke Hekkenberg <thaekeh@gmail.com>
2021-10-02 19:23:51 +0200

    Board with createNewCard first version

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
 import initialBoardData from "../../Data/initialBoardData";
 import { BoardContent } from "../boardContent/BoardContent";
 import { BoardHeader } from "../boardHeader/BoardHeader";
 import "./Board.css";
 
-export const Board = () => {
-  const [boardData] = useState(initialBoardData);
+interface Props {}
+
+export const Board: React.FC<Props> = () => {
+  // const [boardData, setBoardData] = useState(initialBoardData);
+
+  const onCreateNewCard = (cardTitle: string) => {
+    console.log("creating carasdfd");
+    console.log(cardTitle);
+    const ticket = {
+      title: cardTitle,
+      id: "asodijfjoias",
+    };
+    initialBoardData.tickets["asodijfjoias"] = ticket;
+    initialBoardData.columns["column_1"].tickets.push(ticket.id);
+    console.log(initialBoardData);
+    // setBoardData(initialBoardData);
+  };
 
   return (
     <div className="board">
-      <BoardHeader boardTitle={boardData.boardSettings.title} />
-      <BoardContent boardData={boardData} />
+      <BoardHeader boardTitle={initialBoardData.boardSettings.title} />
+      <BoardContent
+        boardData={initialBoardData}
+        onCreateNewCard={onCreateNewCard}
+      />
     </div>
   );
 };
